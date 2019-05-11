234db128de382181a03a39166dfa4f399913f52c
Jlanglang <347891332@qq.com>
2019-05-11 18:10:48 +0800

    1.修改问题

--- module-home/src/main/java/com/baozi/homemodle/ui/activity/MainActivity.java
+++ module-home/src/main/java/com/baozi/homemodle/ui/activity/MainActivity.java
@@ -1,41 +1,18 @@
 package com.baozi.homemodle.ui.activity;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.TextView;
 
 import com.baozi.homemodle.R;
-import com.baozi.homemodle.service.CustomTestService;
 import com.baozi.mvp.presenter.BasePresenter;
 import com.baozi.mvp.presenter.EmptyPresenter;
 import com.baozi.mvp.tempalet.TemplateActivity;
-import com.baozi.mvp.tempalet.helper.SimpleToolbarHelperImpl;
 
 
 public class MainActivity extends TemplateActivity<BasePresenter> {
-
-    private TextView view;
-
-    @Override
-    public SimpleToolbarHelperImpl getToolbarHelper() {
-        return (SimpleToolbarHelperImpl) super.getToolbarHelper();
-    }
-
-    @Override
-    public void onNewThrowable(Throwable throwable) {
-        super.onNewThrowable(throwable);
-    }
-
     @Override
     protected void init(@Nullable Bundle savedInstanceState) {
         super.init(savedInstanceState);
-        view = findView(R.id.tv_content);
-        Intent intent = new Intent(this, CustomTestService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent);
-        }
     }
 
     @Override
